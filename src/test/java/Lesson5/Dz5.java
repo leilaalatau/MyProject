e0516d787ff54e7092011d4596a74c41ac6bef8f
leila <leilaalatau@gmail.com>
2022-12-12 10:32:27 +0600

    n

---
+++ src/test/java/Lesson5/Dz5.java
@@ -0,0 +1,43 @@
+package Lesson5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.concurrent.TimeUnit;
+
+public abstract class Dz5 {
+
+        private static WebDriver driver;
+
+        @BeforeAll
+        static void init(){
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--incognito");
+            //options.addArguments("--headless");
+            options.addArguments("start-maximized");
+            driver = new ChromeDriver(options);
+            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        }
+
+        @BeforeEach
+        void goTo(){
+            Assertions.assertDoesNotThrow( ()-> driver.navigate().to("https://www.vstu.ru"),
+                    "Страница не доступна");
+        }
+
+
+        @AfterAll
+        static void close(){
+            //driver.quit();
+        }
+
+        public static WebDriver getDriver() {
+            return driver;
+        }
+}
